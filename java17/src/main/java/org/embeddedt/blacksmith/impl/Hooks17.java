28fa51d6acc8ba3015bea9127961315adf374177
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-08-24 11:51:19 -0400

    Wrap debug logic in a flag

--- java17/src/main/java/org/embeddedt/blacksmith/impl/Hooks17.java
+++ java17/src/main/java/org/embeddedt/blacksmith/impl/Hooks17.java
@@ -9,8 +9,23 @@ import java.util.List;
 import java.util.Set;
 
 public class Hooks17 {
+    private static final boolean DEBUG = true;
     public static Configuration resolveAndBind(ModuleFinder before, List<Configuration> parents, ModuleFinder after, Collection<String> roots) {
         System.out.println("FAST RESOLVE");
-        return ConfigurationUtil.resolveAndBind(before, parents, after, roots);
+        Configuration c1 = ConfigurationUtil.resolveAndBind(before, parents, after, roots);
+        if(DEBUG) {
+            Configuration c2 = Configuration.resolveAndBind(before, parents, after, roots);
+            String s1 = ConfigurationUtil.dumpConfiguration(c1);
+            String s2 = ConfigurationUtil.dumpConfiguration(c2);
+            if(!s1.equals(s2)) {
+                System.out.println("NOT MATCHED!");
+                System.out.println("Fast:");
+                System.out.println(s1);
+                System.out.println("Slow:");
+                System.out.println(s2);
+                throw new AssertionError();
+            }
+        }
+        return c1;
     }
 }
